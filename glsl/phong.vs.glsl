1122cdaa31dcf14e1de8ffe85207d142cf4f4586
Jack <wozhouchao@hotmail.com>
2016-03-14 16:45:17 -0700

    wrong

--- glsl/phong.vs.glsl
+++ glsl/phong.vs.glsl
@@ -1,16 +1,20 @@
-varying vec4 V_Normal_VCS;
-varying vec4 V_ViewPosition;
+varying vec3 v_Position;       // This will be passed into the fragment shader.
+varying vec4 v_Color;          // This will be passed into the fragment shader.
+varying vec3 v_Normal;         // This will be passed into the fragment shader.
+
 uniform vec3 lightColor;
-uniform vec3 ambientColor;
-uniform vec3 lightPosition;
-uniform float kAmbient;
-uniform float kDiffuse;
-uniform float kSpecular;
-uniform float shininess;
-
-void main() {
-	V_Normal_VCS = vec4(normalize(normalMatrix * normal), 1.0);
-	V_ViewPosition = modelViewMatrix * vec4(position, 1.0);
-
-	gl_Position = projectionMatrix *  modelViewMatrix * vec4(position, 1.0);
+ 
+// The entry point for our vertex shader.
+void main()
+{
+    v_Position = vec3(modelViewMatrix * vec4(position,1.0));
+ 
+    v_Color = vec4(lightColor,1.0);
+ 
+    // Transform the normal's orientation into eye space.
+    v_Normal = vec3(modelViewMatrix * vec4(normal, 0.0));
+ 
+    // gl_Position is a special variable used to store the final position.
+    // Multiply the vertex by the matrix to get the final point in normalized screen coordinates.
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
 }
